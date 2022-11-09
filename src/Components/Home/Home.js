f9d19abca3b78242209272f47bdfeb7319849ebb
capmahin <mahinulhuque1996@gmail.com>
2022-11-09 12:52:38 +0600

    Add Navber is home page

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBars from './NavBars';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBars/>
+        </div>
+    );
+};
+
+export default Home;
