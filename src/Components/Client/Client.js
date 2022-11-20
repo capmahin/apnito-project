5bef34c074dce969ad4cb97345c40021dca4e783
capmahin <mahinulhuque1996@gmail.com>
2022-11-20 12:52:53 +0600

    add clinet and make it responsive and add review button

--- src/Components/Client/Client.js
+++ src/Components/Client/Client.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Clients from './Clients';
-
+import clinet1 from '../../image/assists/clinet/client.jpg'
 
 const clients = [
-  { id: 1, name: "john corner",  },
-  { id: 2, name: "john corner",  },
-  { id: 3, name: "john corner", },
-  { id: 4, name: "john corner",  },
-  { id: 5, name: "john corner",  },
-  { id: 6, name: "john corner", },
+  { id: 1, name: "john corner", img: clinet1 },
+  { id: 2, name: "john corner", img: clinet1 },
+  { id: 3, name: "john corner", img: clinet1 },
+  { id: 4, name: "john corner",  img: clinet1 },
+  { id: 5, name: "john corner",  img: clinet1},
+  { id: 6, name: "john corner", img: clinet1},
 ];
 
 const Client = () => {
--- src/Components/Client/Clients.js
+++ src/Components/Client/Clients.js
@@ -1,17 +1,21 @@
 import React from "react";
+import styles from "./Clinet.module.css";
 
 const Clients = ({ client }) => {
-  const { name } = client;
+  const { name,img } = client;
   return (
     <div className="g-5 col-sm-12 col-md-6 col-lg-4">
       <div className="card " style={{ width: "18rem" }}>
-        {/* <img src={img} className="card-img-top" alt="..." /> */}
+        <img src={img} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h3 className="card-title">{name}</h3>
-          <h5 className="card-text">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, excepturi. At, eius autem fugiat consequuntur et praesentium asperiores? Nam sunt quae provident cupiditate error ipsam, excepturi laborum perspiciatis facilis distinctio.
-          </h5>
-         
+          <h4 className="card-title text-warning">{name}</h4>
+          <h5 className='text-success'>"Comments":</h5>
+          <p className="card-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, excepturi. At, eius autem fugiat 
+          </p>
+          <button className={`${styles["review-button"]} me-4`}>
+              Review
+            </button>
         </div>
       </div>
     </div>
