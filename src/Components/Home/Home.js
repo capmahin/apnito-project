eb0ace8c7ccf4f23e5904734c051cf29d4e6c474
capmahin <mahinulhuque1996@gmail.com>
2022-11-20 13:45:20 +0600

    Add Team component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Client from '../Client/Client';
+import Team from '../Team/Team';
 import About from './About/About';
 import NavBars from './NavBars';
 import Slider from './Slider';
@@ -11,6 +12,7 @@ const Home = () => {
             <Slider/>
             <About/>
             <Client/>
+            <Team/>
         </div>
     );
 };
---
+++ src/Components/Team/Team.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import member1 from '../../image/assists/clinet/client.jpg'
+import Teams from './Teams';
+
+const teams = [
+  { id: 1, name: "john corner", img: member1 },
+  { id: 2, name: "john corner", img: member1 },
+  { id: 3, name: "john corner", img: member1 },
+  { id: 4, name: "john corner",  img: member1 },
+  
+];
+
+const Team = () => {
+  return (
+    <div id="experts" className="container">
+      <h2 className="text-warning text-center mt-4">Our Team</h2>
+      <div className="row">
+        {teams.map((team) => (
+          <Teams key={team.id} team={team}></Teams>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Team;
