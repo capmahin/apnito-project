fd10d34158fb753fa8d7d55a9efaf740f0e297cd
capmahin <mahinulhuque1996@gmail.com>
2022-11-20 11:25:31 +0600

    add color in button

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import styles from "./About.module.css";
 
 const About = () => {
     return (
@@ -9,14 +10,23 @@ const About = () => {
                 <Col xs={4}>
                   <h1 className='  text-warning '>About </h1>
                   <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium, blanditiis odio non qui exercitationem nobis repellendus, architecto nihil voluptatum doloribus distinctio velit. A dignissimos aspernatur cumque doloremque, nemo doloribus!</h5>
+                  <button className={`${styles["explore-button"]} me-4`}>
+              Explore
+            </button>
                </Col>
                <Col xs={4}>
                <h1 className='  text-warning'>Mission</h1>
                <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias possimus accusantium quisquam a eum eos impedit repellat. Natus distinctio temporibus, iste animi enim officia non minus nemo rerum corrupti dolore!</h5>
+               <button className={`${styles["explore-button"]} me-4`}>
+              Explore
+            </button>
                </Col>
                <Col xs={4}>
                <h1 className='text-warning'>Vision</h1>
                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae perferendis in possimus dicta, earum quos deleniti hic ducimus atque exercitationem dolorem, facilis iure? Temporibus dolorum, mollitia eligendi doloribus cupiditate repellat.</h5>
+               <button className={`${styles["explore-button"]} me-4`}>
+              Explore
+            </button>
                </Col>
                 </Row>
 
