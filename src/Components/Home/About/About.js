3982adf6013e47e820f3b2553740b65477a079e5
capmahin <mahinulhuque1996@gmail.com>
2022-11-21 12:50:31 +0600

    change in about section

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <Container className='py-3 '>
                 <Row>
                 <Col xs={6} className="py-5">
-                  <h1 className='  text-warning '>About </h1>
+                  <h1 className='  text-warning text-center'>!! About !! </h1>
                   <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium, blanditiis odio non qui exercitationem nobis repellendus, architecto nihil voluptatum doloribus distinctio velit. A dignissimos aspernatur cumque doloremque, nemo doloribus!</h5>
                   <button className={`${styles["explore-button"]} me-4`}>
               Explore
