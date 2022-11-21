c0757825dad7514677589b4b6b3adff85d69507c
capmahin <mahinulhuque1996@gmail.com>
2022-11-21 12:25:42 +0600

    add px in mission and vission

--- src/Components/Home/About/MissonVission/MIssonVisson.js
+++ src/Components/Home/About/MissonVission/MIssonVisson.js
@@ -4,16 +4,16 @@ import styles from "../About.module.css";
 const MIssonVisson = () => {
     return (
         <div>
-            <Container className='py-3'>
+            <Container className='py-4'>
                 <Row>
-                <Col xs={6}>
+                <Col xs={6} className="px-5">
                   <h1 className='  text-warning '>Mission </h1>
                   <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium, blanditiis odio non qui exercitationem nobis repellendus, architecto nihil voluptatum doloribus distinctio velit. A dignissimos aspernatur cumque doloremque, nemo doloribus!</h5>
                   <button className={`${styles["explore-button"]} me-4`}>
               Explore
             </button>           
                </Col>
-                <Col xs={6}>
+                <Col xs={6} className="px-5">
                   <h1 className='  text-warning '>Vission </h1>
                   <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium, blanditiis odio non qui exercitationem nobis repellendus, architecto nihil voluptatum doloribus distinctio velit. A dignissimos aspernatur cumque doloremque, nemo doloribus!</h5>
                   <button className={`${styles["explore-button"]} me-4`}>
