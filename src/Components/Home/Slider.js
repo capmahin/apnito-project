46f8a8656988052380a15c2e1f7595c9043f0d88
capmahin <mahinulhuque1996@gmail.com>
2022-11-28 12:03:49 +0600

    add

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -1,51 +1,50 @@
-import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
-
+import React from 'react';
+import slider1 from '../../image/assists/slider/Slider1.jpg'
+import slider2 from '../../image/assists/slider/Slider2.jpg'
+import slider3 from '../../image/assists/slider/slider3.jpg'
 
 const Slider = () => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          style={{ height: "700px" }}
-          className="d-block w-100 "
-          src="https://media-exp1.licdn.com/dms/image/C4D22AQHaZWnzrODTTw/feedshare-shrink_800/0/1664683857024?e=2147483647&v=beta&t=ixDfjb5QF9Hx652ECLAoEoxz3-FvIwcQk84-ptQEvMg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-        <h3>Apnito</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ height: "700px" }}
-          className="d-block w-100 "
-          src="https://media-exp1.licdn.com/dms/image/C4D22AQHaZWnzrODTTw/feedshare-shrink_800/0/1664683857024?e=2147483647&v=beta&t=ixDfjb5QF9Hx652ECLAoEoxz3-FvIwcQk84-ptQEvMg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-        <h3>Apnito</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ height: "700px" }}
-          className="d-block w-100 "
-          src="https://media-exp1.licdn.com/dms/image/C4D22AQHaZWnzrODTTw/feedshare-shrink_800/0/1664683857024?e=2147483647&v=beta&t=ixDfjb5QF9Hx652ECLAoEoxz3-FvIwcQk84-ptQEvMg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Apnito</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={slider1} class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={slider2} class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={slider3} class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
   );
 };
 
